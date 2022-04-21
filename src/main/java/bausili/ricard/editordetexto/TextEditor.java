efcd528f927aaa8a9554d6b1ce8df7850567587a
Ricard Bausili <r.bausili@gmail.com>
2022-04-21 17:16:05 +0200

    new

--- src/main/java/bausili/ricard/editordetexto/TextEditor.java
+++ src/main/java/bausili/ricard/editordetexto/TextEditor.java
@@ -1,17 +1,16 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package bausili.ricard.editordetexto;
 
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Font;
+import java.awt.GraphicsEnvironment;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JColorChooser;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JScrollPane;
@@ -21,10 +20,6 @@ import javax.swing.ScrollPaneConstants;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
-/**
- *
- * @author Ricard
- */
 public class TextEditor extends JFrame implements ActionListener {
     
     JTextArea textArea;
@@ -32,25 +27,32 @@ public class TextEditor extends JFrame implements ActionListener {
     JLabel fontLabel;
     JSpinner fontSizeSpinner;
     JButton fontColorButton;
+    JComboBox fontBox;
+    
     
     TextEditor(){
+        //WINDOW
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setTitle("Editor de Texto");
         this.setSize(600,600);
         this.setLayout(new FlowLayout());
         this.setLocationRelativeTo(null);
         
+        //TEXT AREA
         textArea = new JTextArea();
         textArea.setLineWrap(true);
         textArea.setWrapStyleWord(true);
         textArea.setFont(new Font("Calibri", Font.PLAIN,20));  
 
+        //SCROLLPANE textArea
         scrollPane = new JScrollPane(textArea);
         scrollPane.setPreferredSize(new Dimension(550,550));
         scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
         
+        //LABEL fontSize
         fontLabel = new JLabel("Font size: ");
         
+        //SPINNER fontSize
         fontSizeSpinner = new JSpinner();
         fontSizeSpinner.setPreferredSize(new Dimension(50,25));
         fontSizeSpinner.setValue(20);
@@ -60,10 +62,20 @@ public class TextEditor extends JFrame implements ActionListener {
                 textArea.setFont(new Font(textArea.getFont().getFamily(),Font.PLAIN, (int) fontSizeSpinner.getValue()));
             }    
         });
-        
+            
+        //BUTTON fontColor
         fontColorButton = new JButton("Color");
         fontColorButton.addActionListener(this);
+
+        //COMBOBOX fonts
+        String[] fonts = GraphicsEnvironment.getLocalGraphicsEnvironment().getAvailableFontFamilyNames();
         
+        fontBox = new JComboBox(fonts);
+        fontBox = addActionListener(this);
+        fontBox.setSelectedItem("Arial");
+
+        //Añadiendo los items
+        this.add(fontBox);
         this.add(fontColorButton);
         this.add(fontLabel);
         this.add(fontSizeSpinner);
@@ -71,11 +83,15 @@ public class TextEditor extends JFrame implements ActionListener {
         this.setVisible(true);
     }
 
-    @Override
     public void actionPerformed(ActionEvent ae) {
         if(ae.getSource()==fontColorButton){
             Color color = JColorChooser.showDialog(null, "Chooser a color", Color.black);
             textArea.setForeground(color);
         }
+
+        if (ae.getSource()==fontBox) {
+            textArea.setFont(new Font((String)fontBox.getSelectedItem(),Font.PLAIN,textArea.getFont().getSize()));            
+        }
+        
     }
 }
